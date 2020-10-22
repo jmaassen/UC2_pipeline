fa3a7061627afe6a972eb71f73f13c4bcf6a2708
souley <s.madougou@esciencecenter.nl>
2020-10-21 23:09:52 -0400

    Better doc and typos in UC2_pipeline/__init__.py.

--- UC2_pipeline/__init__.py
+++ UC2_pipeline/__init__.py
@@ -2,6 +2,7 @@ import json
 import os
 import subprocess
 import requests
+import re
 
 def give_name():
     jsonfile = give_config()
@@ -27,11 +28,13 @@ def give_argument_names(required=False):
         return properties
 
 
-def run_pipeline(observation, **kargs):
+def run_pipeline(observation, observation2, **kargs):
     print(kargs)
+    print("###Observation: ", observation)
+    print("###Observation2: ", observation2)
     # Request for staging data from LOFAR LTA
     webhook = "http://localhost:8000/sessions"
-#    srmuris = ["srm://srm.grid.sara.nl:8443/pnfs/grid.sara.nl/data/lofar/ops/projects/lofarschool/246403/L246403_SAP000_SB000_uv.MS_7d4aa18f.tar"]
+    #    srmuris = ["srm://srm.grid.sara.nl:8443/pnfs/grid.sara.nl/data/lofar/ops/projects/lofarschool/246403/L246403_SAP000_SB000_uv.MS_7d4aa18f.tar"]
     tarfiles = observation.split("|")
     srmuris = [] #tarfiles[:20] # testing
     for tfile in tarfiles:
@@ -42,7 +45,7 @@ def run_pipeline(observation, **kargs):
     headers = {
         'Content-Type': 'application/json',
     }
-
+    
     data = {
         "id": "staging",
         "cmd": {
@@ -54,11 +57,11 @@ def run_pipeline(observation, **kargs):
             },
             "credentials": {}
         },
-        "webhook": {"method": "post", "url": webhook, "headers": {}},
+        #        "webhook": {"method": "post", "url": webhook, "headers": {}},
         "options": {},
     }
-
-#    print(kargs)
+    
+    #    print(kargs)
     for kw in kargs:
         if kw == "staging":
             url = kargs[kw]["url"] + url
@@ -67,38 +70,11 @@ def run_pipeline(observation, **kargs):
     reqData = json.dumps(data)
     print("===REQ URL=", url)
     print("===REQ DATA=", reqData)
-    res = requests.post(url, headers=headers, data=reqData)
-    print(res)
-    res_data = json.loads(res.content.decode("utf8"))
-    print("Your staging request ID is ", str(res_data["requestId"]))
+#    res = requests.post(url, headers=headers, data=reqData)
+#    print(res)
+#    res_data = json.loads(res.content.decode("utf8"))
+#    print("Your staging request ID is ", str(res_data["requestId"]))
+    res = ""
     return res
 
 
-#def run_pipeline(observation, **kargs):
-#    # Start your pipeline here
-#    url = '/jobs'
-#    headers = {
-#        'Content-Type': 'application/json',
-#    }
-#    data = {
-#        "input": {}
-#    }
-#    #    print(kargs)
-#    for kw in kargs:
-#        if kw == "xenon_server_url":
-#            url = kargs[kw] + url
-#        elif kw == "api_key":
-#            headers["api-key"] = kargs[kw]
-#        elif kw == "workflow_name":
-#            data["name"] = kargs[kw]
-#        elif kw == "workflow_cwl":
-#            data["workflow"] = kargs[kw]
-#    res = requests.post(url, headers=headers, data=json.dumps(data))
-#    # Parse HttpResponse and return xenon-flow job id for later use
-#    # check content of res.data and retrieve res.data['id']???
-#    #    print(res.content)
-#    res_data = json.loads(res.content.decode("utf8"))
-#    #    print("===xenon job id: ", res_data["id"])
-#    res_val = "xenon-flow job id: " + res_data["id"]
-#    return res
-##    return "Testing ..."
